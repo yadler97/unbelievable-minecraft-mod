4ce62c390d56431cf69ccd5848a743a725d8ca82
Yannick Adler <yannick.adler@yahoo.de>
2021-11-28 17:09:29 +0100

    added name tag recipe and changed sapphire tools textures

--- src/main/java/com/yannick/datagen/Recipes.java
+++ src/main/java/com/yannick/datagen/Recipes.java
@@ -3,10 +3,7 @@ package com.yannick.datagen;
 import com.yannick.setup.Registration;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.recipes.FinishedRecipe;
-import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
-import net.minecraft.data.recipes.SimpleCookingRecipeBuilder;
+import net.minecraft.data.recipes.*;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.Items;
 
@@ -26,7 +23,6 @@ public class Recipes extends RecipeProvider {
                 .pattern(" s ")
                 .define('x', Registration.SAPPHIRE.get())
                 .define('s', Items.STICK)
-                .group("examplemod")
                 .unlockedBy("sapphire", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.SAPPHIRE.get()))
                 .save(consumer);
 
@@ -36,7 +32,6 @@ public class Recipes extends RecipeProvider {
                 .pattern(" s ")
                 .define('x', Registration.SAPPHIRE.get())
                 .define('s', Items.STICK)
-                .group("examplemod")
                 .unlockedBy("sapphire", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.SAPPHIRE.get()))
                 .save(consumer);
 
@@ -46,7 +41,6 @@ public class Recipes extends RecipeProvider {
                 .pattern(" s ")
                 .define('x', Registration.SAPPHIRE.get())
                 .define('s', Items.STICK)
-                .group("examplemod")
                 .unlockedBy("sapphire", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.SAPPHIRE.get()))
                 .save(consumer);
 
@@ -56,7 +50,6 @@ public class Recipes extends RecipeProvider {
                 .pattern(" s ")
                 .define('x', Registration.SAPPHIRE.get())
                 .define('s', Items.STICK)
-                .group("examplemod")
                 .unlockedBy("sapphire", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.SAPPHIRE.get()))
                 .save(consumer);
 
@@ -66,8 +59,14 @@ public class Recipes extends RecipeProvider {
                 .pattern(" s ")
                 .define('x', Registration.SAPPHIRE.get())
                 .define('s', Items.STICK)
-                .group("examplemod")
                 .unlockedBy("sapphire", InventoryChangeTrigger.TriggerInstance.hasItems(Registration.SAPPHIRE.get()))
+                .save(consumer);
+
+        ShapelessRecipeBuilder.shapeless(Items.NAME_TAG)
+                .requires(Items.PAPER)
+                .requires(Items.STRING)
+                .requires(Items.INK_SAC)
+                .unlockedBy("paper", InventoryChangeTrigger.TriggerInstance.hasItems(Items.PAPER))
                 .save(consumer);
 
         SimpleCookingRecipeBuilder.smelting(Ingredient.of(Items.EGG), Registration.FRIED_EGG.get(), 0.35F, 200).unlockedBy("egg", has(Items.EGG)).save(consumer);
